238f066810d5f17d8c48e793d6c2e947cfb96ffa
beske.k <beske.k@husky.neu.edu>
2019-09-16 16:42:09 -0400

    javascript basics

---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+let doWhat = 'amaze';
+let who = 'me';
+console.log(doWhat + ' ' + who);
+
+function multiply(num1,num2) {
+    let result = num1 * num2;
+    return result;
+}
+
+document.querySelector('p').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName || myName === null) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
